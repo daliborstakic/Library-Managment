deb418e7c7935e643afec2b099314af5f97013be
daliborstakic <stakicdalibor64@gmail.com>
2020-10-11 16:39:13 +0200

    Add books getter and setter

--- src/com/daliborstakic/librarymanagment/Library.java
+++ src/com/daliborstakic/librarymanagment/Library.java
@@ -1,15 +1,23 @@
 package com.daliborstakic.librarymanagment;
 
-import java.lang.String;
+import java.lang.String; // Importing String
 import java.util.ArrayList; // Importing the ArrayList
 
 // Library class
 public class Library {
 	public String name;
-	//private ArrayList<Book> books;
+	private ArrayList<Book> books;
 	
 	public Library(String name, ArrayList<Book> books) {
 		this.name = name;
-		//this.books = books;
+		this.setBooks(books);
+	}
+
+	public ArrayList<Book> getBooks() { // books Getter
+		return books;
+	}
+
+	public void setBooks(ArrayList<Book> books) { // books Setter
+		this.books = books;
 	}
 }
